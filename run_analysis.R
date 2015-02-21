762b1207dd796345587347506c4e78b8d4b8d47d
Sean Stephenson <sms@pixar.com>
2015-02-21 15:42:50 -0800

    add answer 1 and stub out features

--- run_analysis.R
+++ run_analysis.R
@@ -1,24 +1,40 @@
 ################################################################################
 getTest <- function() {
 ################################################################################
-  test <- read.table("./data/UCI_HAR_Dataset/test/X_test.txt")
+  subj_test <- read.table("./data/UCI_HAR_Dataset/test/subject_test.txt")
+  X_test    <- read.table("./data/UCI_HAR_Dataset/test/X_test.txt")
+  y_test    <- read.table("./data/UCI_HAR_Dataset/test/y_test.txt")
+  test      <- cbind(subj_test, y_test, X_test)
   return(test)
 }
 
 ################################################################################
-getTraining <- function() {
+getTrain <- function() {
 ################################################################################
-  training <- read.table("./data/UCI_HAR_Dataset/train/X_train.txt")
-  return(training)
+  subj_train <- read.table("./data/UCI_HAR_Dataset/train/subject_train.txt")
+  X_train    <- read.table("./data/UCI_HAR_Dataset/train/X_train.txt")
+  y_train    <- read.table("./data/UCI_HAR_Dataset/train/y_train.txt")
+  train      <- cbind(subj_train, y_train, X_train)
+  return(train)
 }
 
-#
-# 1. Merges the training and the test sets to create one data set.
-#
 ################################################################################
-mergeTestAndTraining <- function() {
+getFeatures <- function() {
 ################################################################################
-  test <- getTest()
-  training <- getTraining()
+  features <- read.table("./data/UCI_HAR_Dataset/features.txt")
+  return(features)
+}
+
+################################################################################
+main <- function() {
+################################################################################
+  test  <- getTest()
+  train <- getTrain()
+
+  # 1. Merge the training and the test sets to create one data set.
   rbind(test, training)
+
+  # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+  features <- getFeatures()
+  features <- grep("mean|std", features$V2)
 }
