4186e56bb57dc28cb114e558f4e1962452901694
Sean Stephenson <sms@pixar.com>
2015-02-21 16:42:06 -0800

    get qs 1, 2, 3

--- run_analysis.R
+++ run_analysis.R
@@ -1,42 +1,24 @@
 ################################################################################
-getTest <- function() {
-################################################################################
-  subj_test   <- read.table("./data/UCI_HAR_Dataset/test/subject_test.txt")
-  X_test      <- read.table("./data/UCI_HAR_Dataset/test/X_test.txt")
-  y_test      <- read.table("./data/UCI_HAR_Dataset/test/y_test.txt")
-  test        <- cbind(subj_test, y_test, X_test)
-  return(test)
-}
-
-################################################################################
-getTrain <- function() {
-################################################################################
-  subj_train  <- read.table("./data/UCI_HAR_Dataset/train/subject_train.txt")
-  X_train     <- read.table("./data/UCI_HAR_Dataset/train/X_train.txt")
-  y_train     <- read.table("./data/UCI_HAR_Dataset/train/y_train.txt")
-  train       <- cbind(subj_train, y_train, X_train)
-  return(train)
-}
-
-################################################################################
 getFeatures <- function() {
 ################################################################################
-  features    <- read.table("./data/UCI_HAR_Dataset/features.txt")
+  features <- read.table("./data/UCI_HAR_Dataset/features.txt")
   return(features)
 }
 
 ################################################################################
 main <- function() {
 ################################################################################
-  test        <- getTest()
-  train       <- getTrain()
-  features    <- getFeatures()
+  features <- getFeatures()
+  headers  <- features$V2
 
-  # 1. Merge the training and the test sets to create one data set.
-  data_set    <- rbind(test, training)
+  # Appropriately labels the data set with descriptive variable names.
+  test     <- read.table("./data/UCI_HAR_Dataset/test/X_test.txt",   col.names = headers)
+  train    <- read.table("./data/UCI_HAR_Dataset/train/X_train.txt", col.names = headers)
 
-  # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-  measuremnts <- features[grep("mean|std", features$V2),]
+  # Merge the training and the test sets to create one data set.
+  data <- rbind(test, train)
 
-  # 3. Use descriptive activity names to name the activities in the data set
+  # Extract only the measuremnts on the mean and standard deviation for each measurement.
+  measuremnts <- features[grep("mean|std", features$V2),]$V2
+  data[,measurements]
 }
